15f0516c10f7162e97ee0a16b1e84315cfd8e7a8
Nahl Basalamah <nahlultra@gmail.com>
2022-03-18 10:44:53 +0700

    init : dzikr_app initial commit

---
+++ android/app/src/main/kotlin/com/example/dzikr_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dzikr_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:dzikr_app/pages/home_page/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}

5474acf8695c220a6ede37d495337409688f2a3b
Nahl Basalamah <nahlultra@gmail.com>
2022-04-22 15:13:54 +0700

    try :  fix package error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:dzikr_app/core/config/theme_config.dart';
 import 'package:dzikr_app/pages/spash_page/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(
     MaterialApp(
       title: 'Dzikir App',
